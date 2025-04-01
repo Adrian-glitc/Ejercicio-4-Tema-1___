e98312bcda0e7346c7ae51512fe3316b49746951
Adrian-glitc <amadridm@student.universidadunie.com>
2025-04-01 17:11:07 +0200

    cambios

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+
+# Ejemplo de uso
+if __name__ == "__main__":
+    cola = ColaMisiones()
+
+    cola.agregar_mision(Mision("Rescatar al aldeano", 3))
+    cola.agregar_mision(Mision("Recolectar hierbas", 1))
+    cola.agregar_mision(Mision("Derrotar al dragón", 5))
+
+    print("Misiones en cola:")
+    cola.mostrar_misiones()
+
+    print("\nSiguiente misión:")
+    print(cola.obtener_siguiente_mision())
+
+    print("\nMisiones restantes:")
+    cola.mostrar_misiones()
